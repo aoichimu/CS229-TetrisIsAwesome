314699d43394e3215b18194a3633028e4f12cc38
Jiaming Zeng <jiamingzeng@DN51o04c.SUNet>
2016-11-21 18:58:05 -0800

    Updated with target model

--- CS-229 RL/simpleDQN.py
+++ CS-229 RL/simpleDQN.py
@@ -12,12 +12,13 @@ import gym
 from collections import deque
 import random
 
-from keras.models import Sequential
+from keras.models import Sequential, model_from_config
 from keras.layers import Dense, Activation, Flatten
-from keras.optimizers import RMSProp, sgd, 
+from keras.optimizers import RMSprop, sgd, Adam
 
 ENV_NAME = 'Breakout-ram-v0'
-os.chdir('/home/edgard/Desktop/CS229-TetrisIsAwesome/CS-229 RL')
+#os.chdir('/home/edgard/Desktop/CS229-TetrisIsAwesome/CS-229 RL')
+os.chdir('/Users/jiamingzeng/Dropbox/Stanford/CS 229/Project/CS229-TetrisIsAwesome/CS-229 RL')
 
 # Get the environment and extract the number of actions.
 env = gym.make(ENV_NAME)
@@ -25,6 +26,7 @@ np.random.seed(123)
 env.seed(123)
 env.reset()
 
+################# FUNCTIONS ################
 # Sets the frameskip and a Game Over signal to train and if testing, it plays the game normally.
 mode='train'
 def _step(a):
@@ -42,7 +44,16 @@ env._step = _step
 nb_actions = env.action_space.n
 state_size = env.observation_space.shape
 
-# Setting values
+def clone_model(model, custom_objects={}):
+    config = {
+        'class_name': model.__class__.__name__,
+        'config': model.get_config(),
+    }
+    clone = model_from_config(config, custom_objects=custom_objects)
+    clone.set_weights(model.get_weights())
+    return clone
+    
+################# MODEL INITIALIZATION AND PARAMETERS ################
 gamma = 0.99 # decay rate of past observations
 warmup = 10000 # timesteps to observe before training
 explore = 1000 # frames over which to anneal epsilon
@@ -51,15 +62,17 @@ epsilon_t0 = 0.1 # starting value of epsilon
 memory_replay = warmup # number of previous transitions to remember
 batch_size = 32 # size of minibatch
 nb_steps = 1000000
+update_target = 1000
+train_visualize = False
 #FRAME_PER_ACTION = 1
 
 # Initialize model 
 
 model = Sequential()
 model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-model.add(Dense(128))
+model.add(Dense(128,init='uniform'))
 model.add(Activation('relu'))
-model.add(Dense(128))
+model.add(Dense(128,init='uniform'))
 model.add(Activation('relu'))
 model.add(Dense(nb_actions))
 model.add(Activation('linear'))
@@ -77,17 +90,24 @@ model.add(Activation('linear'))
 
 print(model.summary())
 
+# Initialize target model
+target_model = clone_model(model)
+target_model.compile(Adam(lr=0.1), 'mse')
+#print(target_model.summary())
+
 #model.compile(sgd(lr=0.2, clipvalue=1), 'mse')
 #model.compile(Adam(lr=0.001, clipvalue=1), 'mse')
-model.compile(RMSProp, 'mse')
+model.compile(Adam(lr=0.1), 'mse')
 
 ################# TRAINING ################
 
 # initialize action value function q
 action_t0 = env.action_space.sample()
-env.render()
+if train_visualize:
+    env.render()
 state_t0, reward, terminal, info = env.step(action_t0)
 
+# Start training
 epsilon = epsilon_t0
 state_t = state_t0
 
@@ -105,8 +125,8 @@ while t < nb_steps:
     # Initialize outputs
     loss = 0
     rr = 0
-    max_Q = 0
     action = 0
+    max_Q = 0
     
     # Reshape state_t
     state_t = state_t.reshape(1, 1, state_t0.shape[0])
@@ -116,11 +136,11 @@ while t < nb_steps:
         action = env.action_space.sample()
     else:
         q = model.predict(state_t)
-        max_Q = np.argmax(q)
-        action = max_Q
+        action = np.argmax(q)
     
     # Carry out action and observe new state state_t1 and reward
-    env.render()
+    if train_visualize:
+        env.render()
     state_t1, reward, terminal, info = env.step(action)
     state_t1 = state_t1.reshape(1, 1, state_t0.shape[0])
     if terminal:
@@ -160,12 +180,15 @@ while t < nb_steps:
         loss += model.train_on_batch(qInputs, targets)
         all_loss.write('\n' + str(loss))
         all_Q.write('\n' + str(max_Q))
-    
+        
+        # Update target model
+        if (t % update_target == 0):
+            target_model.set_weights(model.get_weights())
     t += 1
     state_t = state_t1
     
     # Save weights and output periodically
-    if (t % 1000 ==0):
+    if (t % 1000 == 0):
         print("Time", t, "Loss ", '%.2E' % loss, "Max Q", max_Q, "Action ", action)
         model.save_weights('dqn_{}_params.h5f'.format(ENV_NAME), overwrite=True)
 
@@ -224,3 +247,12 @@ for eps in range(1, episodes+1):
 #agent = Agent(model);
 #agent.train(env)
 #agent.test(env)
+
+#def frameStep(visualize, action):
+#    if visualize:
+#        env.render()
+#    state, reward, terminal, info = env.step(action)
+#    state_t = state.reshape(1, 1, state.shape[0])
+#    if terminal:
+#        env.reset()
+#    return [state_t, reward, terminal, info]
