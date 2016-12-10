6d1e1f264f06a0a65909e2261481825b7144054c
Jiaming Zeng <jiamingzeng@DN51o035.SUNet>
2016-12-09 20:35:11 -0800

    update with new networks

--- CS-229-RL/ADQN_RAM/model_RAM.py
+++ CS-229-RL/ADQN_RAM/model_RAM.py
@@ -3,12 +3,64 @@ from keras import backend as K
 from keras.layers import Convolution2D, Flatten, Dense, Input
 from keras.models import Model
 
-def build_network(num_actions):
-  with tf.device("/cpu:0"):
-    state = tf.placeholder("float", [None, 128])
-    inputs = Input(shape=(128,))
-    model = Dense(256, activation='relu')(inputs)
-    model =  Dense(256, activation='relu')(model)
-    q_values = Dense(output_dim=num_actions, activation='linear')(model)
-    m = Model(input=inputs, output=q_values)
-  return state, m
+def build_network(num_actions, network):
+  if network == 1:
+    with tf.device("/cpu:0"):
+      state = tf.placeholder("float", [None, 128])
+      inputs = Input(shape=(128,))
+      model = Dense(256, activation='relu')(inputs)
+      model =  Dense(256, activation='relu')(model)
+      q_values = Dense(output_dim=num_actions, activation='linear')(model)
+      m = Model(input=inputs, output=q_values)
+    return state, m
+  elif network == 2:
+    with tf.device("/cpu:0"):
+      state = tf.placeholder("float", [None, 128])
+      inputs = Input(shape=(128,))
+      model = Dense(128, activation='relu')(inputs)
+      model =  Dense(128, activation='relu')(model)
+      q_values = Dense(output_dim=num_actions, activation='linear')(model)
+      m = Model(input=inputs, output=q_values)
+    return state, m
+  elif network == 3:
+    with tf.device("/cpu:0"):
+      state = tf.placeholder("float", [None, 128])
+      inputs = Input(shape=(128,))
+      model = Dense(256, activation='relu')(inputs)
+      model =  Dense(256, activation='relu')(model)
+      model =  Dense(256, activation='relu')(model)
+      q_values = Dense(output_dim=num_actions, activation='linear')(model)
+      m = Model(input=inputs, output=q_values)
+    return state, m
+  elif network == 4:
+    with tf.device("/cpu:0"):
+      state = tf.placeholder("float", [None, 128])
+      inputs = Input(shape=(128,))
+      model = Dense(128, activation='relu')(inputs)
+      model =  Dense(128, activation='relu')(model)
+      model =  Dense(128, activation='relu')(model)
+      q_values = Dense(output_dim=num_actions, activation='linear')(model)
+      m = Model(input=inputs, output=q_values)
+    return state, m
+  elif network == 5:
+    with tf.device("/cpu:0"):
+      state = tf.placeholder("float", [None, 128])
+      inputs = Input(shape=(128,))
+      model = Dense(256, activation='relu')(inputs)
+      model =  Dense(256, activation='relu')(model)
+      model =  Dense(256, activation='relu')(model)
+      model =  Dense(256, activation='relu')(model)
+      q_values = Dense(output_dim=num_actions, activation='linear')(model)
+      m = Model(input=inputs, output=q_values)
+    return state, m
+  else:
+    with tf.device("/cpu:0"):
+      state = tf.placeholder("float", [None, 128])
+      inputs = Input(shape=(128,))
+      model = Dense(128, activation='relu')(inputs)
+      model =  Dense(128, activation='relu')(model)
+      model =  Dense(128, activation='relu')(model)
+      model =  Dense(128, activation='relu')(model)
+      q_values = Dense(output_dim=num_actions, activation='linear')(model)
+      m = Model(input=inputs, output=q_values)
+    return state, m
