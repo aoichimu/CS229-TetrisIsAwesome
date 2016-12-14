df5ece5eae4026929cef280554303bd3947fce90
eddbonilla <edgard@stanford.edu>
2016-12-13 23:50:38 -0800

    Please... I wanna sleep

--- PAPER_TEST/trainingBQ1.sh
+++ PAPER_TEST/trainingBQ1.sh
@@ -13,9 +13,10 @@
 #$ -m ae
 #
 ##Computer parameters 
+CS229_dir="/farmshare/user_data/edgard/CS229-TetrisIsAwesome/CS-229-RL/PAPER_TEST/"
+cd ${CS229_dir}
 
-
-python async_dqn_RAM_Boltzmann.py
+python Boltz-f=1/async_dqn_RAM_Boltzmann.py
 #python  Boltz-f=1-2/async_dqn_RAM_Boltzmann.py
 #python Boltz-f=1-3/async_dqn_RAM_Boltzmann.py
 #python Epsilon/async_dqn_RAM.py
