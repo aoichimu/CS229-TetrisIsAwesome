b505d0a002c0279acb9a48ae262726283c07aa15
eddbonilla <edgard@stanford.edu>
2016-12-13 23:40:25 -0800

    Please... I wanna sleep

--- PAPER_TEST/runFinal.sh
+++ PAPER_TEST/runFinal.sh
@@ -1,5 +1,9 @@
+cd Boltz-f=1
 qsub -l h_rt=100:00:00 -l longq=1 trainingBQ1.sh
+cd ../Boltz-f=1-2
 qsub -l h_rt=100:00:00 -l longq=1 trainingBQ1-2.sh
+cd ../Boltz-f=1-3
 qsub -l h_rt=100:00:00 -l longq=1 trainingBQ1-3.sh
+cd ../Epsilon
 qsub -l h_rt=100:00:00 -l longq=1 trainingE.sh
 
