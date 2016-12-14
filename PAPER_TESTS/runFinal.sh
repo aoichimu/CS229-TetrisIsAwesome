74eec893b5a81f8e497ab0d92d4603947526e96e
eddbonilla <edgard@stanford.edu>
2016-12-13 23:08:54 -0800

    final testing

---
+++ PAPER_TESTS/runFinal.sh
@@ -0,0 +1,4 @@
+qsub -l h_rt=100:00:00 -l longq=1 trainingBQ1.sh
+qsub -l h_rt=100:00:00 -l longq=1 trainingBQ1-2.sh
+qsub -l h_rt=100:00:00 -l longq=1 trainingBQ1-3.sh
+qsub -l h_rt=100:00:00 -l longq=1 trainingE.sh
---
+++ PAPER_TESTS/trainingBQ1.sh
@@ -0,0 +1,21 @@
+#Shell Options
+#!/bin/bash
+#        
+#Machine Options
+#
+#Log File
+#$ -j y
+#$ -o /farmshare/user_data/edgard/Logs/
+#$ -e /farmshare/user_data/edgard/Logs/
+#
+#Send an e-mail to edgard when job is aborted
+#$ -M edgard@stanford.edu
+#$ -m ae
+#
+##Computer parameters 
+
+
+python ./Boltz-f=1/async_dqn.py
+#python ./Boltz-f=1-2/trainingBQ1-2.py
+#python ./Boltz-f=1-3/trainingBQ1-3.py
+#python ./Epsilon/trainingE.py
