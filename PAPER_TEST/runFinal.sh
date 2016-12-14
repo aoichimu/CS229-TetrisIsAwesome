9890a44a6358aa590afeea31e3dc9ea6478a1fef
eddbonilla <edgard@stanford.edu>
2016-12-13 23:46:24 -0800

    Please... I wanna sleep

--- PAPER_TEST/runFinal.sh
+++ PAPER_TEST/runFinal.sh
@@ -1,3 +1,5 @@
+CS229_dir="/farmshare/user_data/edgard/CS229-TetrisIsAwesome/CS-229-RL/PAPER_TEST/"
+cd ${CS229_dir}
 cd Boltz-f=1
 qsub -l h_rt=100:00:00 -l longq=1 trainingBQ1.sh
 cd ../Boltz-f=1-2
