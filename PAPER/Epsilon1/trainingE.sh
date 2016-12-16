55fcc42a573412d4f4e34220b8794ef3e705939c
Edgard Luis Bonilla <edgard@corn08.stanford.edu>
2016-12-15 20:25:41 -0800

    Partial_test_1

---
+++ PAPER/Epsilon1/trainingE.sh
@@ -0,0 +1,22 @@
+#Shell Options
+#!/bin/bash
+#        
+#Machine Options
+#
+#Log File
+#$ -j y
+#$ -o /farmshare/user_data/edgard/Logs/
+#$ -e /farmshare/user_data/edgard/Logs/
+#
+#Send an e-mail to edgard when job is aborted
+#$ -M edgard@stanford.edu
+#$ -m ae
+#
+##Computer parameters 
+CS229_dir="/farmshare/user_data/edgard/CS229-TetrisIsAwesome/PAPER/Epsilon1/"
+cd ${CS229_dir}
+
+#python Boltz-f=1/async_dqn_RAM_Boltzmann.py
+#python  Boltz-f=1-2/async_dqn_RAM_Boltzmann.py
+#python Boltz-f=1-3/async_dqn_RAM_Boltzmann.py
+python async_dqn_RAM_Epsilon.py
